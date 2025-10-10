fcc2928340b0f102e6784af9df68b99365b000bc
Pablo12331 <130071914+Pablo12331@users.noreply.github.com>
2025-10-10 04:35:01 +0000

    Mover logica de login al Auth

--- src/alumno/alumno/alumno.controller.ts
+++ src/alumno/alumno/alumno.controller.ts
@@ -4,11 +4,5 @@ import { AlumnoService } from './alumno.service';
 @Controller({})
 export class AlumnoController 
 {
-    constructor(private alumno: AlumnoService) {}
 
-    @Post('/login')
-    getData()
-    {
-        return this.alumno.getAlumno("pedro@example.com", "qwerty");
-    }
 }
--- src/auth/auth/auth.controller.ts
+++ src/auth/auth/auth.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+class LoginDto
+{
+    email: string;
+    password: string;
+}
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController 
+{
+    constructor(private auth: AuthService) {}
+
+    @Post('/login')
+    login(@Body() loginDto: LoginDto)
+    {
+        return this.auth.login(loginDto.email, loginDto.password);
+    }
+    
+}
