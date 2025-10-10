884ca12980776f9fe295f5e37a5df938fa513f88
Pablo12331 <130071914+Pablo12331@users.noreply.github.com>
2025-10-10 01:23:52 +0000

    Cambios Alumno

--- src/alumno/alumno/alumno.controller.ts
+++ src/alumno/alumno/alumno.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { AlumnoService } from './alumno.service';
 
-@Controller('alumno')
-export class AlumnoController {}
+@Controller()
+export class AlumnoController 
+{
+    constructor(private alumno: AlumnoService) {}
+    @Get('/login')
+    getData(email: string, contraseña: string)
+    {
+        return this.alumno.getAlumno(email, contraseña);
+    }
+}
--- src/alumno/alumno/alumno.service.ts
+++ src/alumno/alumno/alumno.service.ts
@@ -1,4 +1,57 @@
 import { Injectable } from '@nestjs/common';
 
+
+export interface LoginResponse
+{
+    rut: string;
+    carreras: 
+    {
+        codigo:string;
+        nombre:string;
+        catalogo:string;
+    }[];
+}
+export interface ErrorResponse 
+{
+  error: string;
+}
+
 @Injectable()
-export class AlumnoService {}
+export class AlumnoService 
+{
+   async fetchloginData(email: string, password: string): Promise<LoginResponse> 
+    {
+        const url = `https://puclaro.ucn.cl/eross/avance/login.php?email=${email}&password=${password}`;
+
+        try 
+        {
+            const response = await fetch(url);
+
+            if (!response.ok) 
+            {
+                throw new Error(`Error de red o servidor: ${response.status} ${response.statusText}`);
+            }
+
+            const data: LoginResponse | ErrorResponse = await response.json();
+
+            if ('error' in data) 
+            {
+                throw new Error(data.error || 'Credenciales incorrectas.');
+            }
+
+            return data;
+        } 
+        catch (error) 
+        {
+            console.error(`[fetchLoginData] Falló la petición: ${error.message}`);
+            throw error;
+        }
+    }
+
+    async getAlumno(email: string, contraseña: string)
+    {
+        const alumno = await this.fetchloginData(email, contraseña);
+        return alumno;
+    }
+}
+
