759fa56bdb6ecc2dc1bf00c615be2862b2a7276b
Pablo12331 <130071914+Pablo12331@users.noreply.github.com>
2025-10-12 04:48:29 +0000

    Entrega de mallas separadas por semestre, fech de avance

--- src/avance/avance/avance.controller.ts
+++ src/avance/avance/avance.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { AvanceService } from './avance.service';
 
 @Controller('avance')
-export class AvanceController {}
+export class AvanceController 
+{
+    constructor(private avance: AvanceService){}
+    @Get(':rutAlumno/:codigoCarrera')
+    getAvance
+    (
+        @Param('rutAlumno') rutAlumno:string,
+        @Param('codigoCarrera') codigoCarrera:string,
+    )
+    {
+        return this.avance.getAvance(rutAlumno,codigoCarrera);
+    }
+}
--- src/mallacurricular/malla/malla.controller.ts
+++ src/mallacurricular/malla/malla.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { MallaService } from './malla.service';
 
 @Controller('malla')
-export class MallaController {}
+export class MallaController 
+{
+    constructor(private malla: MallaService){}
+
+    @Get(':codigoCarrera/:catalogo')
+    getMalla
+    (
+        @Param('codigoCarrera') codigoCarrera: string,
+        @Param('catalogo') catalogo: string,
+    )
+    {
+        return this.malla.getMalla(codigoCarrera, catalogo);
+    }
+}
