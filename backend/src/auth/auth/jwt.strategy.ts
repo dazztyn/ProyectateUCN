9352d21626c43ab287169716cbd53ab392f36702
Pablo12331 <pablo.jorquera@alumnos.ucn.cl>
2025-10-14 18:04:17 -0300

    Cambio de parametro

--- backend/src/auth/auth/jwt.strategy.ts
+++ backend/src/auth/auth/jwt.strategy.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
+import { Usuario } from 'src/ArchivosComunes/Usuario';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -21,7 +22,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: any) {
+  async validate(payload: Usuario) 
+  {
     return { rut: payload.rut, carreras: payload.carreras };
   }
 }
