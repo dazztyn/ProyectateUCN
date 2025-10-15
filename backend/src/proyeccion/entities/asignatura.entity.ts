33bc52eb4adf18a82dc95e4d3c0f27322456d79e
Pablo12331 <130071914+Pablo12331@users.noreply.github.com>
2025-10-15 19:26:03 +0000

    implementacion db

---
+++ backend/src/proyeccion/entities/asignatura.entity.ts
@@ -0,0 +1,26 @@
+import { Entity, PrimaryColumn, Column, ManyToOne } from 'typeorm';
+import { Semestre } from './semestre.entity'; // Importa la entidad padre
+
+@Entity('asignaturas')
+export class Asignatura {
+  @PrimaryColumn({ type: 'text', nullable: false })
+  codigoAsignatura: string;
+
+  @Column({ type: 'text', nullable: false })
+  nombreAsignatura: string;
+
+  @Column({ type: 'int', nullable: false })
+  creditos: number;
+
+  @Column({ type: 'int', nullable: false })
+  nivel: number;
+
+  @Column({ type: 'text', nullable: false })
+  prerrequisitos: string;
+
+  // --- RELACIÓN CON SEMESTRE ---
+  // Muchas Asignaturas pertenecen a un Semestre.
+  // Este lado tendrá la columna de la llave foránea ('semestreIdSemestre').
+  @ManyToOne(() => Semestre, (semestre) => semestre.asignaturas)
+  semestre: Semestre;
+}
